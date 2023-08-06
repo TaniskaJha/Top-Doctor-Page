77f1b64dd107400c150f37eb1a96612684db2ac7
Taniska Jha <112795163+TaniskaJha@users.noreply.github.com>
2023-08-06 23:59:18 +0530

    Update page.js

--- Top-Doctors-Page/app/page.js
+++ Top-Doctors-Page/app/page.js
@@ -8,7 +8,7 @@ const Page = () => (
     <div className="relative">
       <About />
       <div className="gradient-03 z-0" />
-      <Explore />
+    
     </div>
     <div className="relative">
       <GetStarted />
@@ -21,6 +21,7 @@ const Page = () => (
       <div className="gradient-04 z-0" />
       <Feedback />
     </div>
+     <Explore />
     <Footer />
   </div>
 );
