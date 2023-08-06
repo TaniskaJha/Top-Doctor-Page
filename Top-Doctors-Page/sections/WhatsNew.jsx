b48bf00940e17047b4105c5ab4f4c01b757ca724
Taniska Jha <112795163+TaniskaJha@users.noreply.github.com>
2023-08-07 00:57:05 +0530

    Update WhatsNew.jsx

--- Top-Doctors-Page/sections/WhatsNew.jsx
+++ Top-Doctors-Page/sections/WhatsNew.jsx
@@ -1,39 +1,47 @@
 import { motion } from 'framer-motion';
 import styles from '../styles';
-import { planetVariants, staggerContainer, fadeIn, textVariant } from '../utils/motion';
+import { staggerContainer, textVariant } from '../utils/motion';
 
-const WhatsNew = () => (
+const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
     <motion.div
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <div className="flex justify-center items-center gap-2">
-        <div className="flex-1">
-          <motion.h1 variants={textVariant(1.1)} className={`${styles.heroHeading} mb-2`}>
-            <span className="cis">Why </span>
-            <span className="iq">chose </span>
-            <span className="cis">us?</span>
-          </motion.h1>
+      <div className="flex justify-center items-center gap-2 ">
+        <div className="flex-1 ">
+          <div className="flex justify-center items-center flex-col"> {/* Centering */}
+            <motion.h1 variants={textVariant(1.1)} className={`${styles.heroHeading} mb-2`}>
+              <span className="cis">Why </span>
+              <span className="iq">choose </span>
+              <span className="cis">us?</span>
+            </motion.h1>
 
-          <motion.h1 variants={textVariant(1.1)} className={styles.SubHeading}>
-            <span className="cis">We want you and your heart to be healthy so you can live a happy life!</span>
-          </motion.h1>
+            <motion.h1 variants={textVariant(1.1)} className={styles.SubHeading}>
+              <span className="cis">We want you and your heart to be healthy so you can live a happy life!</span>
+            </motion.h1>
+          </div>
         </div>
       </div>
 
-      <motion.div
-        variants={planetVariants('right')}
-        className={`flex-1 ${styles.flexCenter}`}
-        style={{ textAlign: 'right' }}
-      >
-        <img src="/Female.png" alt="get-started" className="w-[500px] h-[900px] object-contain" />
-      </motion.div>
+      <div className="flex flex-row gap-2 mt-8">
+        <motion.img
+          src="j.png"
+          alt="Image at the top"
+          className="w-[740px] h-[505px] float-left mr-3 mb-3 rounded-[10px]"
+        />
+        <motion.img
+          src="Female.png"
+          alt="Image at the top"
+          className="w-[740px] h-[605px] float-left mr-3 mb-3 rounded-[10px]"
+        />
+      </div>
     </motion.div>
   </section>
 );
 
-export default WhatsNew;
+export default About;
